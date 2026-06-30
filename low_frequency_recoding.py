1bd931fb91ef6551152ba4d072b8763b0d963c14
Eran Maor <eran.maor.em@gmail.com>
2026-06-29 21:24:12 -0700

    06/29: low frequency capture

---
+++ low_frequency_recoding.py
@@ -0,0 +1,48 @@
+from enum import auto
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+from scipy.signal import stft, iirfilter
+from scipy.fft import fft, fftfreq, fftshift, ifft, rfft, rfftfreq, irfft
+import math
+
+def transform(file_name):
+    sample_rate, data = wavfile.read(file_name)
+    length=len(data)/2
+    f = rfftfreq(len(data), 1/sample_rate)
+
+    if len(data.shape) > 1:
+        # It's stereo: average the left (channel 0) and right (channel 1) to make it mono
+        mono_data = (data[:, 0].astype(float) + data[:, 1].astype(float)) / 2
+    else:
+        # It's already mono
+        mono_data = data.astype(float)
+
+    f_transform = rfft(mono_data)
+    amplitude = np.abs(f_transform) / length
+
+
+    return data, f_transform, amplitude, f, sample_rate
+
+def plot(x, y, title, xlabel, ylabel):
+    plt.figure(figsize=(10, 4))
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.plot(x, y)
+    plt.tight_layout()
+    plt.show()
+
+def plot_freqs(x, y, title, xlabel, ylabel):
+    plt.figure(figsize=(10, 4))
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.xlim(0, 100)
+    #plt.semilogx(x, y)
+    plt.plot(x, y)
+    plt.tight_layout()
+    plt.show()
+
+data, f_transform, amplitude, f, sample_rate = transform('40hz.wav')
+plot_freqs(f, amplitude,  'Magnitude vs Frequencies', 'Frequency [Hz]', 'Magnitude')
--- main.py
+++
@@ -1,9 +0,0 @@
-from plot import plot
-from transform import transform
-from transform import remove_dc_offset
-
-right_limit = 23000
-
-left_data, f_transform_left, f_left, amplitude_left, right_data, f_transform_right, f_right, amplitude_right = transform('a_milli.wav')
-plot(f_left, amplitude_left,  'Left A Milli - Amplitude vs Frequencies (pre-shift)', 'Frequency [Hz]', 'Amplitude', [0, right_limit])
-plot(f_right, amplitude_right,  'Right A Milli - Amplitude vs Frequencies (pre-shift)', 'Frequency [Hz]', 'Amplitude', [0, right_limit])
